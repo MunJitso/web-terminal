7592a4f5981d8d5680fa69cd82754734eecfa91b
MunJitso <72764494+MunJitso@users.noreply.github.com>
2022-08-21 22:56:49 +0100

    Updating

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Terminal from "./Terminal";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <div className="terminal">
+          <div className="hot-bar">
+            <div>
+              <p className="window-name"> Root Terminal</p>
+            </div>
+            <div className="buttons">
+              <div className="minimize"></div>
+              <div className="fullscreen"></div>
+              <div className="close"></div>
+            </div>
+          </div>
+          <div className="infos">
+              <Terminal/>
+          </div>
+        </div>
+      </div>
   );
 }
+
 
 export default App;
---
+++ src/Terminal.js
@@ -0,0 +1,79 @@
+import React, {useEffect, useRef, useState} from 'react';
+import './Terminal.css';
+
+
+function PromptInput() {
+    const [output, setOutput] = useState();
+    const [input, setInput] = useState("");
+    const inputRef = useRef()
+    useEffect(() => {
+        inputRef.current.focus()
+    },[])
+    let isTrue = false;
+    return(
+        <>
+            <p>root:munjitso:$ ~ <input ref={inputRef} type={"text"} className={"input"} value={input}
+                                        onChange={e => setInput(e.target.value)} readOnly={isTrue} onKeyDown={(e) => {commandChecker(e, input, setOutput, setInput); }}/></p>
+            {output}
+        </>
+    )
+
+}
+
+function commandChecker (event, input, setOutput, setInput) {
+    if (event.key === 'Enter') {
+        setOutput("");
+        switch (input) {
+            case "info" :
+                setOutput(<><Info/></>);
+                break;
+            case "clear" :
+                setOutput("");
+                break;
+            case "exit" :
+                window.close();
+            default :
+                setOutput(<><p>use 'help' to know commands.</p>
+                </>);
+                break;
+        }
+        setInput("");
+    }
+}
+function Info() {
+    return (
+        <div>
+            <div className="header">
+                <h1>MunJitso</h1>
+                <p>does not have any skill at all.</p>
+            </div>
+            <div className='body'>
+                <div className="info-paragraph">
+                    <p>I'm 16 years old, self-taught, school sucks. I have lot of interests
+                        such as web dev, making desktop applications, programming bots, and some other stuff
+                        outside
+                        programming like physics, math, typology...</p>
+                </div>
+                <p>Some programming languages I know: </p>
+                <ul className="list">
+                    <li>Java</li>
+                    <li>Python</li>
+                    <li>JavaScript</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+function Terminal() {
+    return (
+        <div className="container">
+            <div className="terminal">
+                <div className="infos">
+                    <PromptInput/>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Terminal;
